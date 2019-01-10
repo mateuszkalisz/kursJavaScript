2b8801e4929543cc42f87c66dc04cca7761d37eb
MK <kaliszmat1@gmail.com>
2019-01-10 20:43:20 +0100

    lesson 1 - type of variables

---
+++ jsKurs/task1/code.js
@@ -0,0 +1,44 @@
+console.log("start");
+console.log(2 + 2);
+let result = 2 + 2;
+"tekst".toUpperCase();
+const users = [];
+let points = 10;
+
+typeof "tekst";
+typeof "22222";
+typeof 'blablabla';
+let userName = "Tymoteusz";
+typeof userName;
+typeof 22;
+typeof 22.2;
+typeof `grawis`;
+typeof NaN;
+console.log(typeof 33.2e-1);
+console.log(NaN);
+typeof true;
+console.log(typeof 2 == 3);
+console.log(3e321321412535135151 * 9e999999999999999);
+
+typeof undefined;
+let dateFirstLogin;
+typeof dateFirstLogin;
+
+typeof null;
+
+typeof Symbol();
+
+typeof {};
+typeof [];
+typeof
+
+function () {};
+
+let primitiveType1 = 20;
+let primitiveType2 = primitiveType1;
+primitiveType1 = 30;
+
+let referenceValue1 = [1, 2, 3];
+let referenceValue2 = referenceValue1;
+referenceValue1[3] = 4;
+referenceValue2[4] = 10;
