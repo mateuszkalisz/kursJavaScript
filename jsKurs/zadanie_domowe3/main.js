1f42fc3caa9f32cecd67cd27b869cbc84d489a6c
MK <kaliszmat1@gmail.com>
2019-02-05 22:06:05 +0100

    show list and grow this list size after click

---
+++ jsKurs/zadanie_domowe3/main.js
@@ -0,0 +1,33 @@
+//1 SPOSOB - PETLA FOREACH
+
+// const liElements = document.body.querySelectorAll("ul>li");
+// const btn = document.body.querySelector("button");
+// let size = 10;
+
+// function changeSize() {
+//     liElements.forEach((liElement) => {
+//         liElement.style.display = "block";
+//         liElement.style.fontSize = size + "px";
+//     })
+//     size += 2;
+// }
+
+// btn.addEventListener("click", changeSize);
+
+//2 SPOSOB - PETLA FOR
+
+const liElements = document.body.querySelectorAll("ul>li");
+const btn = document.body.querySelector("button");
+let size = 10;
+
+function changeSize() {
+
+    for (let i = 0; i < liElements.length; i++) {
+        liElements[i].style.display = "block";
+        liElements[i].style.fontSize = size + "px";
+    }
+
+    size += 2;
+}
+
+btn.addEventListener("click", changeSize);
