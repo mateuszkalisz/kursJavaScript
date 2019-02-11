e00523dabc48d0d8e9dca7333b3a29214e06abbb
MK <kaliszmat1@gmail.com>
2019-02-11 22:22:14 +0100

    change background colour after mouse move action

--- jsKurs/Projekt3_WyswietlaniePozycjiKursora/main.js
+++ jsKurs/Projekt3_WyswietlaniePozycjiKursora/main.js
@@ -1,7 +1,9 @@
 const h1 = document.body.querySelector("h1")
 
 document.body.addEventListener("mousemove", (e) => {
-    //h1.textContent = `${e.clientX}, ${e.clientY}`;
+    h1.textContent = `${e.clientX}, ${e.clientY}`;
     //h1.textContent = `${e.pageX}, ${e.pageY}`;
-    h1.textContent = `${e.screen}, ${e.screen}`;
+    //h1.textContent = `${e.screen}, ${e.screen}`;
+    document.body.style.backgroundColor = `rgb(${e.clientX/2}, ${e.clientY/3}, ${e.clientX/e.clientY*20})`;
+
 })
