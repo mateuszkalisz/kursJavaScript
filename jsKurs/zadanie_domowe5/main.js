0123d04289f8ac1680e96f8cdc45c89bd8edb7c6
MK <kaliszmat1@gmail.com>
2019-02-13 23:46:52 +0100

    homework

---
+++ jsKurs/zadanie_domowe5/main.js
@@ -0,0 +1,17 @@
+document.body.addEventListener("click", (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+
+
+    if (x % 2 == 0 && y % 2 == 0) {
+        document.body.style.backgroundColor = "blue";
+
+    } else if (x % 2 != 0 && y % 2 != 0) {
+        document.body.style.backgroundColor = "red";
+
+    } else {
+        document.body.style.backgroundColor = "green";
+
+    }
+
+})
