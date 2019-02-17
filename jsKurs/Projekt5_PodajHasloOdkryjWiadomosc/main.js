4a099f85deb9391732eb9b52924d13f0e6569f3d
MK <kaliszmat1@gmail.com>
2019-02-17 14:14:07 +0100

    several passwords

--- jsKurs/Projekt5_PodajHasloOdkryjWiadomosc/main.js
+++ jsKurs/Projekt5_PodajHasloOdkryjWiadomosc/main.js
@@ -1,19 +1,25 @@
 const input = document.getElementById("pass");
 
-const password = "user";
-const message = "wyjechalam na zawsze";
+const passwords = ["user", "wiosna", "ania"];
+const messages = ["wyjechalam na zawsze", "piekna pora roku", "fajne imie masz"];
 const div = document.body.querySelector(".message");
 
 
-input.addEventListener("input", (e) => {
-    console.log(e.target.value);
+input.addEventListener("input", function (e) {
 
-    if (password === e.target.value) {
-        div.textContent = message;
-        e.target.value = "";
-    } else {
-        div.textContent = "";
-    }
+    div.textContent = "";
+    //console.log(e.target.value);
+    //console.log(this.value);
+    const text = e.target.value;
+    console.log(text);
+
+    passwords.forEach((password, index) => {
+        if (password === text) {
+            div.textContent = messages[index];
+            e.target.value = "";
+        }
+    })
+
 })
 
 input.addEventListener("focus", (e) => {
@@ -24,3 +30,13 @@ input.addEventListener("blur", (e) => {
     e.target.classList.remove("active");
     //e.target.classList.toggle("active");
 })
+
+//funkcja strzałkowa nie ma wlasnego this, this jest dziedziczone a nie tworzone
+//jezeli chcemy uzyc this to najlepiej zadeklarowac funkcję za pomocą słówka this
+
+// if (password === e.target.value) {
+//     div.textContent = message;
+//     e.target.value = "";
+// } else {
+//     div.textContent = "";
+// }
