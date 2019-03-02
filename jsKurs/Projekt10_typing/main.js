9e42569641d26156ec2904e2e476b43155165529
MK <kaliszmat1@gmail.com>
2019-03-02 17:29:10 +0100

    homework 9 - not finish

--- jsKurs/Projekt10_typing/main.js
+++ jsKurs/Projekt10_typing/main.js
@@ -1,13 +1,21 @@
 const spnText = document.querySelector(".text");
-const txt = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pellentesque auctor erat ut cursus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse felis dui, lobortis non risus ac, tempor laoreet nibh. Pellentesque commodo, neque et finibus sollicitudin, nunc orci egestas lacus, sit amet accumsan magna massa in arcu. Fusce rutrum pulvinar dolor, non lacinia justo fringilla vel. Quisque sed neque maximus libero congue pharetra faucibus sagittis nisi. Vestibulum in malesuada nisl. Vestibulum vestibulum ex et tincidunt bibendum."
+const spnCursor = document.querySelector(".cursor");
+const txt = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pellentesque auctor erat ut cursus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse felis dui, lobortis non risus ac, tempor laoreet nibh. Pellentesque commodo, neque et finibus sollicitudin, nunc orci egestas lacus, sit amet accumsan magna massa in arcu. Fusce rutrum pulvinar dolor, non lacinia justo fringilla vel."
 
 let indexText = 0;
-const time = 40;
+const time = 30;
 
 const addLetter = function () {
-    spnText.textContent += txt[indexText]
+    spnText.textContent += txt[indexText];
     indexText++;
     if (indexText === txt.length) clearInterval(indexTyping);
 }
 
 const indexTyping = setInterval(addLetter, time);
+
+
+const cursorAnimation = () => {
+    spnCursor.classList.toggle("active");
+}
+
+setInterval(cursorAnimation, 400);
