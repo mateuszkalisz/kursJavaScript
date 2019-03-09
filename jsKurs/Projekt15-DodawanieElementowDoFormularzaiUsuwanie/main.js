8f66b3ce562be24b30202bb5ae2f5605669bb67d
MK <kaliszmat1@gmail.com>
2019-03-09 15:23:07 +0100

    toDoList - array for application list

--- jsKurs/Projekt15-DodawanieElementowDoFormularzaiUsuwanie/main.js
+++ jsKurs/Projekt15-DodawanieElementowDoFormularzaiUsuwanie/main.js
@@ -1,13 +1,27 @@
+const toDoList = [];
+
 const form = document.querySelector("form");
 const ul = document.querySelector("ul");
 const taskNumber = document.querySelector("h1 span");
 const listItems = document.getElementsByClassName("task");
 const input = document.querySelector("input");
 
+const renderList = () => {
+    ul.textContent = "";
+    toDoList.forEach((toDoElement, key) => {
+        toDoElement.dataset.key = key;
+        ul.appendChild(toDoElement)
+    });
+}
 
 const removeTask = (e) => {
     e.target.parentNode.remove();
+    const index = e.target.parentNode.dataset.key;
+    toDoList.splice(index, 1)
+    console.log(toDoList);
     taskNumber.textContent = listItems.length;
+
+    renderList();
 }
 
 const addTask = (e) => {
@@ -17,6 +31,10 @@ const addTask = (e) => {
     const newLi = document.createElement("li");
     newLi.className = "task";
     newLi.innerHTML = titleTask + "<button>Usuń</button>";
+    toDoList.push(newLi);
+
+    renderList();
+
     ul.appendChild(newLi);
     input.value = "";
     taskNumber.textContent = listItems.length;
