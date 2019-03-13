485f5ff0002a86e21fcc729062dbdcbf2059480f
MK <kaliszmat1@gmail.com>
2019-03-13 22:34:20 +0100

    intro to prototypes

--- jsKurs/OOP1-Konstruktor_i_instancje/main.js
+++ jsKurs/OOP1-Konstruktor_i_instancje/main.js
@@ -1,5 +1,7 @@
+////////////////KONSTRUKTORY I INSTACJE/////////////////////
+
 const Car = function (name) {
-    console.log(this);
+    //console.log(this);
     this.name = name;
 }
 
@@ -11,3 +13,30 @@ const maluch = new Car("maluch");
 const polonez = new Car("polonez");
 
 maluch.name = "zabytek";
+
+
+///////////////PROTOTYPY///////////////
+
+const Person = function (name, age) {
+    this.name = name;
+    this.age = age;
+    this.children = [];
+
+    // this.addChildren = function (name) {
+    //     this.children.push(name);
+    // }
+}
+
+Person.prototype.addChildren = function (name) {
+    this.children.push(name);
+}
+
+//staramy sie nie przypisywac wlasciwosci tylko metody bo nie chcemy zeby np wszystkie obiekty mialy gender = female
+//Person.prototype.gender = "female";
+
+
+
+const arek = new Person("Arek", 20);
+const monika = new Person("Monika", 30);
+
+arek.addChildren("Asia");
