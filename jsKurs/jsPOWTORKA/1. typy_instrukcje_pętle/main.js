d927f1545e69d231f84c3e2f6a881fb8c4d65d4f
MK <kaliszmat1@gmail.com>
2019-07-10 00:03:31 +0200

    small repetition of JS

---
+++ jsKurs/jsPOWTORKA/1. typy_instrukcje_pętle/main.js
@@ -0,0 +1,60 @@
+const h1 = document.querySelector("h1");
+console.log("start");
+
+// let result = 2+2;
+
+// const users = [1,2,3,4,5];
+
+// users[5] = 6;
+
+// let a,b,c;
+
+// a = 10;
+// b = 20;
+// c = 30;
+
+// const name = "Johny walker";
+
+// const txt = name.toUpperCase();
+// const txt2 = name.toLowerCase();
+
+
+// const dl = txt.length;
+
+// const array = [2,5,6,8,9];
+
+// const index = Math.round(Math.random()*array.length)
+
+// h1.textContent = array[index];
+
+// const userName = "Mateusz";
+
+// const indexOf2 = userName.charAt(2);
+
+// h1.textContent = indexOf2;
+
+
+// const userName = "Mateusz";
+// const addText = " to dobry czlowiek";
+
+// const wholeText = userName.concat(addText);
+
+// const indexOfE = wholeText.indexOf("e");
+
+// const lastIndexOfE = wholeText.lastIndexOf("e");
+
+// const includesUsz = userName.includes("usz");
+
+// h1.textContent = includesUsz;
+
+
+// const afterTrim = "      slowo    ";
+
+// console.log(afterTrim);
+
+
+const sliceWord = "Mateusz".slice(1,4);
+
+h1.textContent = sliceWord;
+
+
